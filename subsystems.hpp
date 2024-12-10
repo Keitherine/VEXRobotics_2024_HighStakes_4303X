9f429918961598397bf1194bd904b20977def5fd
Keitherine <160787462+Keitherine@users.noreply.github.com>
2024-12-10 15:41:37 -0500

    Update subsystems.hpp

--- subsystems.hpp
+++ subsystems.hpp
@@ -1,10 +1,21 @@
 #pragma once
-#include <vector>
-#include "api.h"
+#include "EZ-Template/drive/drive.hpp"
 #include "pros/adi.hpp"
+#include "pros/motor_group.hpp"
 
+inline ez::Drive chassis
+(
+    {-5, -7, 13}, // Left Motors, reversed
+    {14, -15, 16}, // Right Motors
+    //top motors are flipped (13 & 15)
+
+    2, // IMU Port
+    3.25,  // Wheel Diameter
+    600 // Wheel RPM
+);   
 inline pros::adi::Pneumatics cl4mp('A', false);
-//inline pros::Motor roll1(9);
-//inline pros::Motor roll2(10);
-//inline pros::Motor hook(11);
-inline pros::MotorGroup intake({1, 2, 3});
+inline pros::Motor ouroboros(10);
+//inline pros::MotorGroup ouroboros({-10, 19});
+
+inline pros::Motor ladyBrown(19); //check/change port
+inline pros::adi::Pneumatics doink('B', false); //check/change port
